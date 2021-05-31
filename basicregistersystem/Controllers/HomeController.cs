6d38a1da05e4798d87c4f815a1eb3b86f3fa4969
asrinutku <auyagmur@hotmail.com>
2021-06-01 02:33:06 +0300

    basicregistersystem

---
+++ basicregistersystem/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace registerproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Models.RegisterModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Session["user"] = model; 
+                return RedirectToAction("Index");
+
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ basicregistersystem/Models/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace registerproject.Models
+{
+    public class RegisterModel
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        
+        public String Password { get; set; }
+        
+
+        [Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor")]
+        
+
+        public String RePassword { get; set; }
+
+
+
+
+
+    }
+}
